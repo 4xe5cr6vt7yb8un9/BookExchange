aade5d1094c9c39efb44d295d1e328cfd54e37d7
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-04-01 21:17:10 -0400

    Created seperate files for get and set sql methods

---
+++ BookExchange/SQLGetActions.cs
@@ -0,0 +1,137 @@
+﻿using System.Data.SqlClient;
+
+namespace BookExchange
+{
+    public static class SQLGetActions
+    {
+        private const String SQLDetails = "Data Source=" +
+                                          "Provider=SQLOLEDB.1;" +
+                                          "Integrated Security=SSPI;" +
+                                          "Persist Security Info=False;" +
+                                          "Initial Catalog=BookExchange;" +
+                                          "Data Source=localhost\\SQLEXPRESS";
+
+        public static Book getBookByQuery(String query)
+        {
+            String searchQuery = "SELECT * FROM Books WHERE " + query;
+            Book selectBook;
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            SqlDataReader sqlReader;
+            try
+            {
+                sqlReader = selectCommand.ExecuteReader();
+
+                if (sqlReader.Read())
+                {
+                    selectBook = new Book
+                    {
+                        Name = sqlReader.GetString(0),
+                        Author = sqlReader.GetString(1),
+                        Description = sqlReader.GetString(2),
+                        Published = sqlReader.GetString(3),
+                        available = Int32.Parse(sqlReader.GetString(4)),
+                        ISBN = sqlReader.GetString(5)
+                    };
+                    return selectBook;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting SELECT.");
+            }
+            selectCommand.Connection.Close();
+            return new Book();
+        }
+
+        public static ExchangeUser getUserByQuery(String query)
+        {
+            String searchQuery = "SELECT * FROM Users WHERE " + query;
+            ExchangeUser selectUser;
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            SqlDataReader sqlReader;
+            try
+            {
+                sqlReader = selectCommand.ExecuteReader();
+
+                if (sqlReader.Read())
+                {
+                    selectUser = new ExchangeUser
+                    {
+                        UserID = sqlReader.GetString(0),
+                        Name = sqlReader.GetString(1),
+                        Email = sqlReader.GetString(2),
+                    };
+                    return selectUser;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting SELECT.");
+            }
+            selectCommand.Connection.Close();
+            return new ExchangeUser();
+        }
+
+        public static List<string> getBorrowByUser(String userID)
+        {
+            String searchQuery = "SELECT Book FROM Borrowers WHERE UserID = " + userID;
+            List<String> books = new();
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            SqlDataReader sqlReader;
+            try
+            {
+                sqlReader = selectCommand.ExecuteReader();
+
+                while (sqlReader.Read())
+                {
+                    _ = books.Append(sqlReader.GetString(0));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting SELECT.");
+            }
+            selectCommand.Connection.Close();
+            return books;
+        }
+
+        public static List<string> getLoansByUser(String userID)
+        {
+            String searchQuery = "SELECT Book FROM Loaners WHERE UserID = " + userID;
+            List<String> books = new();
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            SqlDataReader sqlReader;
+            try
+            {
+                sqlReader = selectCommand.ExecuteReader();
+
+                while (sqlReader.Read())
+                {
+                    _ = books.Append(sqlReader.GetString(0));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting SELECT.");
+            }
+            selectCommand.Connection.Close();
+            return books;
+        }
+    }
+}
---
+++ BookExchange/SQLSetActions.cs
@@ -0,0 +1,111 @@
+﻿using System.Data.SqlClient;
+
+namespace BookExchange
+{
+    public class SQLSetActions
+    {
+        private const String SQLDetails = "Data Source=" +
+                                          "Provider=SQLOLEDB.1;" +
+                                          "Integrated Security=SSPI;" +
+                                          "Persist Security Info=False;" +
+                                          "Initial Catalog=BookExchange;" +
+                                          "Data Source=localhost\\SQLEXPRESS";
+
+        public static void addBook(Book newBook)
+        {
+            String searchQuery = "INSERT INTO Books (ISBN, Title, Author, Descr, Published, Stock) " +
+                                 "VALUES ("+
+                                    newBook.ISBN+"," +
+                                    newBook.Name+"," +
+                                    newBook.Author+"," +
+                                    newBook.Description+"," +
+                                    newBook.Published +"," +
+                                    newBook.available+
+                                 ")";
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            try
+            {
+                selectCommand.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting INSERT.");
+            }
+            selectCommand.Connection.Close();
+        }
+
+        public static void addUser(ExchangeUser newUser)
+        {
+            String searchQuery = "INSERT INTO Books (UserID, Name, Email) " +
+                                 "VALUES (" +
+                                    newUser.UserID + "," +
+                                    newUser.Name + "," +
+                                    newUser.Email + 
+                                 ")";
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            try
+            {
+                selectCommand.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting INSERT.");
+            }
+            selectCommand.Connection.Close();
+        }
+
+        public static void addBorrowedBook(String userID, String book)
+        {
+            String searchQuery = "INSERT INTO Borrowers (UserID, Book) " +
+                                 "VALUES (" +
+                                    userID + "," +
+                                    book + 
+                                 ")";
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            try
+            {
+                selectCommand.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting INSERT.");
+            }
+            selectCommand.Connection.Close();
+        }
+
+        public static void addLoanedBook(String userID, String book)
+        {
+            String searchQuery = "INSERT INTO Loaners (UserID, Book) " +
+                                 "VALUES (" +
+                                    userID + "," +
+                                    book +
+                                 ")";
+
+            using SqlConnection newConnection = new(SQLDetails);
+            SqlCommand selectCommand = new(searchQuery, newConnection);
+            selectCommand.Connection.Open();
+
+            try
+            {
+                selectCommand.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Error occurred while attempting INSERT.");
+            }
+            selectCommand.Connection.Close();
+        }
+    }
+}
