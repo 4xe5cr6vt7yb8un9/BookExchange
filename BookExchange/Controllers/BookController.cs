e81c7adce6cdf3db5bccdb698d71701eb8290927
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-04-01 21:18:00 -0400

    Added Get Queries to controller

--- BookExchange/Controllers/BookController.cs
+++ BookExchange/Controllers/BookController.cs
@@ -1,3 +1,4 @@
+using BookExchange.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookExchange.Controllers
@@ -6,17 +7,45 @@ namespace BookExchange.Controllers
     [Route("[controller]")]
     public class BookController : ControllerBase
     {
+        /*
         private readonly ILogger<BookController> _logger;
 
         public BookController(ILogger<BookController> logger)
         {
             _logger = logger;
+        }*/
+
+        [HttpGet("ISBN/{ISBN}")]
+        public Book GetISBNBook(String ISBN)
+        {
+            Book ISBNBook = SQLGetActions.getBookByQuery("ISBN = "+ISBN);
+            return ISBNBook;
         }
 
-        [HttpGet(Name = "GetBookByISBN")]
-        public Book Get()
+        [HttpGet("TITLE/{Title}")]
+        public Book GetTitleBook(String Title)
         {
-            return new Book();
+            Book TitleBook = SQLGetActions.getBookByQuery("Title = " + Title);
+            return TitleBook;
+        }
+    }
+
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("UserID/{UserID}")]
+        public ExchangeUser GetIDUser(String UserID)
+        {
+            ExchangeUser User = SQLGetActions.getUserByQuery("UserID = " + UserID);
+            return User;
+        }
+
+        [HttpGet("Email/{Email}")]
+        public ExchangeUser GetEmailUser(String Email)
+        {
+            ExchangeUser User = SQLGetActions.getUserByQuery("Email = " + Email);
+            return User;
         }
     }
 }
