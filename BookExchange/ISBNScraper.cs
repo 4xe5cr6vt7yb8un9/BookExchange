7c8fb405ce715ffd77285a298fd2b9e187ff6c1f
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-04-02 10:56:32 -0400

    added a scrapper for adding books using the ISBN

---
+++ BookExchange/ISBNScraper.cs
@@ -0,0 +1,64 @@
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using System.Globalization;
+using ScrapySharp.Extensions;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+
+namespace BookExchange
+{
+    public class ISBNScraper
+    {
+        public static Book Scrap(String ISBN)
+        {
+            //Send the request to the server
+            ISBN = ISBN.Replace(" ", String.Empty).Replace("-", String.Empty);
+
+            HtmlWeb web = new HtmlWeb();
+
+            HtmlDocument doc = web.Load("http://api.scraperapi.com?api_key=39a77dc21bd643280e402b3741f4e780&url=https://isbnsearch.org/isbn/" + ISBN);
+
+            //Select a specific node from the HTML doc
+            var image = doc.DocumentNode.CssSelect("div.image > img");
+            var header = doc.DocumentNode.CssSelect("div.bookinfo > h1");
+            var bookinfo = doc.DocumentNode.CssSelect("div.bookinfo > p");
+
+            //Extract the text and store it in a CSV file
+
+            Book newBook = new();
+            newBook.ISBN = ISBN;
+
+            foreach (var item in image)
+            {
+                String imageUrl = item.GetAttributeValue("src");
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(imageUrl, "C:\\Users\\aiden\\source\\repos\\BookExchange\\Data\\ICONS\\"+ISBN+".jpg");
+            }
+
+            foreach (var item in header)
+            {
+                newBook.Title = item.InnerText;
+            }
+
+            foreach (var item in bookinfo)
+            {
+                switch (item.FirstChild.InnerText)
+                {
+                    case "Author:":
+                        String author = item.LastChild.InnerText.Replace("\"", String.Empty).Trim();
+                        newBook.Author = author;
+                        break;
+                    case "Published:":
+                        String published = item.LastChild.InnerText.Replace("\"", String.Empty).Trim();
+                        newBook.Published = published;
+                        break;
+                }
+            }
+            return newBook;
+        }
+
+    }
+}
