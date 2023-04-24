1a108b4702862c4c8c069efbddb3f0f3b75994aa
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-04-24 11:21:14 -0400

    Added Required

--- BookExchange/Models/Book.cs
+++ BookExchange/Models/Book.cs
@@ -1,5 +1,7 @@
 using Google.Apis.Books.v1.Data;
 using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace BookExchange.Models
 {
@@ -7,14 +9,20 @@ namespace BookExchange.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Book Title is Required")]
+        [Display(Name = "Book Title")]
         public String Title { get; set; }
 
+        [Required(ErrorMessage = "Book Author is Required")]
+        [Display(Name = "Authors")]
         public String Author { get; set; }
 
-        public String Description { get; set; }
+        public String? Description { get; set; }
 
-        public String Published { get; set; } 
+        public String? Published { get; set; }
 
+        [Required(ErrorMessage = "Book ISBN is Required")]
+        [Display(Name = "ISBN-13")]
         public String ISBN { get; set; }
 
         public int Available { get; set; }
@@ -24,12 +32,11 @@ namespace BookExchange.Models
     public class ISBNData
     {
         public String Title { get; set; }
-        public String Subtitle { get; set; }
+        public String? Subtitle { get; set; }
         public IList<String> Authors { get; set; }
-        public String Published_date { get; set; }
-        public String Description { get; set; }
+        public String? Published_date { get; set; }
+        public String? Description { get; set; }
         public IList<Volume.VolumeInfoData.IndustryIdentifiersData> IndustryIdentifiers { get; set; }
-        public Volume.VolumeInfoData.ImageLinksData ImageLinks { get; set; }
-
+        public Volume.VolumeInfoData.ImageLinksData? ImageLinks { get; set; }
     }
 }
