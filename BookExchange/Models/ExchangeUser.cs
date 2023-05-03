617bc8221155f6aff05a34b9cb2745b177e52a41
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-05-03 10:49:20 -0400

    Added Detail to loaned Books and Rented Books

--- BookExchange/Models/ExchangeUser.cs
+++ BookExchange/Models/ExchangeUser.cs
@@ -1,4 +1,7 @@
-﻿namespace BookExchange.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BookExchange.Models
 {
     public class ExchangeUser
     {
@@ -12,11 +15,34 @@
     public class Loans
     {
         public Guid Id { get; set; }
+
+        [Display(Name = "Loaner Name")]
+        public String LoanerName { get; set; }
+
+        [Display(Name = "Loaner Email")]
+        public String LoanerEmail { get; set; }
+
         public String ISBN { get; set; }
+
+        [Display(Name = "Loaned Date")]
+        public DateTime LoanDate { get; set; }
     }
-    public class Borrowed
+    public class Rents
     {
         public Guid Id { get; set; }
+
+        [Display(Name = "Renter Name")]
+        public String RenterName { get; set; }
+
+        [Display(Name = "Renter Email")]
+        public String RenterEmail { get; set; }
+
+        [Display(Name = "Rented From")]
+        public String RentedFrom { get; set; }
+
         public String ISBN { get; set; }
+
+        [Display(Name = "Rented Date")]
+        public DateTime RentDate { get; set; }
     }
 }
