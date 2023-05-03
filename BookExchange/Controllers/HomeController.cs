22f8b80c8fc8923365f0318f20e33159841421b6
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-05-03 10:50:59 -0400

    Added route for page not found error

--- BookExchange/Controllers/HomeController.cs
+++ BookExchange/Controllers/HomeController.cs
@@ -30,5 +30,12 @@ namespace BookExchange.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [Route("/PageNotFound")]
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+
     }
 }
