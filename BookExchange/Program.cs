d1c206abbdea2b98a77b38df427ebaa81b67082d
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-05-22 14:46:05 -0400

    Changed Book routing

--- BookExchange/Program.cs
+++ BookExchange/Program.cs
@@ -49,10 +49,22 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-//app.MapControllers();
+
+app.MapControllerRoute(
+    name: "Books",
+    pattern: "Books/{startIndex}",
+    defaults: new
+    {
+        controller = "Books",
+        action = "Page",
+        startIndex = 1,
+        pageSize = 6
+    }
+    );
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
