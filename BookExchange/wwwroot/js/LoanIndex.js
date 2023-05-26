6dc3388591f9d04849338f32eca8965841f58645
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-05-26 08:24:04 -0400

    Changed Color of Links when hovering

---
+++ BookExchange/wwwroot/js/LoanIndex.js
@@ -0,0 +1,10 @@
+﻿'strict'
+
+$(document).ready(function () {
+    $('tr').hover(function () {
+        $(this).find('a').css('color', 'white');
+    }, function () {
+        $(this).find('a').css('color', 'black');
+        $('.rent').css('color', 'mediumblue');
+    })
+})
