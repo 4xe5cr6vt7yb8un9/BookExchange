63b4cae65b73379b52799f9d5cb7fd442128002e
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-05-30 12:36:19 -0400

    Centers Page

--- BookExchange/wwwroot/js/BookIndex.js
+++ BookExchange/wwwroot/js/BookIndex.js
@@ -2,4 +2,17 @@
 
 $(document).ready(function () {
     $(".disabled").removeAttr("href")
+    centerMain()
+
+    $(window).resize(function () {
+        centerMain()
+    });
+
+    function centerMain() {
+        let main = document.querySelector('.main')
+        let container = document.querySelector('.container')
+        let marg = ((container.offsetHeight - 10) / 2 - main.offsetHeight / 2) + 'px'
+
+        $(".main").css("top", marg)
+    }
 })
