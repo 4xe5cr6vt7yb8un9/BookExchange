26459cca1d21a8ca1c2f8a606072efb34412b070
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-06-13 10:21:18 -0400

    Model to Tie books with classes

---
+++ BookExchange/Models/BookClassIndex.cs
@@ -0,0 +1,9 @@
+﻿namespace BookExchange.Models
+{
+    public class BookClassIndex
+    {
+        public PaginatedList<Book> Books { get; set;}
+        public IEnumerable<Classes> Classes { get; set;}
+    }
+
+}
