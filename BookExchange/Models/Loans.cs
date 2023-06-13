9062e4c77e8a5707dab2d4ec66a6fd0ad3714423
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-06-13 10:22:35 -0400

    Moved Loans to its own model

---
+++ BookExchange/Models/Loans.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookExchange.Models
+{
+    public class Loans
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public String LoanerName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public String LoanerEmail { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone number")]
+        public String LoanerPhone { get; set; }
+
+        public Guid BookID { get; set; }
+
+        [Required]
+        [NotMapped]
+        [StringLength(13, ErrorMessage = "ISBN must be less than 13 characters long")]
+        [Display(Name = "Book ISBN")]
+        public String ISBN { get; set; }
+
+        [NotMapped]
+        public String title { get; set; }
+
+        [Display(Name = "Date Donated")]
+        public DateTime LoanDate { get; set; }
+    }
+}
