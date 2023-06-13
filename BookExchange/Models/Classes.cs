1144e93bd704de73a6b99cdff40fda369db58055
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-06-13 10:22:08 -0400

    Create Class Model

---
+++ BookExchange/Models/Classes.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookExchange.Models
+{
+    public class Classes
+    {
+        [Key]
+        public Guid ClassID { get; set; }
+
+        [Display(Name="Class Name")]
+        [Required(ErrorMessage = "Class Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Class Grade")]
+        [Required(ErrorMessage = "Class Grade is required")]
+        public int Grade { get; set; }
+
+        [Display(Name = "Class Teacher")]
+        [Required(ErrorMessage = "Class Teacher is required")]
+        public string Teacher { get; set; }
+    }
+
+    public class ClassBook
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        public Guid ClassID { get; set; }
+
+        public Guid BookID { get; set; }
+    }
+}
