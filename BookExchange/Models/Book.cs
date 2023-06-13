d7e85c9e12fbc9f4d0893b771bbf58eb9ecda346
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2023-06-13 10:21:05 -0400

    Added book Subtitle to Model Split ISBN into ISBN13 and ISBN10 Created Custom model for CreateISBN

--- BookExchange/Models/Book.cs
+++ BookExchange/Models/Book.cs
@@ -1,16 +1,23 @@
 using Google.Apis.Books.v1.Data;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookExchange.Models
 {
+    [Index(nameof(ISBN13), IsUnique = true)]
+    [Index(nameof(ISBN10), IsUnique = true)]
+
     public class Book
     {
-        //public int Id { get; set; }
         public Guid BookID { get; set; }
 
         [Required(ErrorMessage = "Book Title is Required")]
-        [Display(Name = "Book Title")]
+        [Display(Name = "Title")]
         public String Title { get; set; }
+
+        [Display(Name = "Subtitle")]
+        public String? Subtitle { get; set; }
 
         [Required(ErrorMessage = "Book Author is Required")]
         [Display(Name = "Authors")]
@@ -20,19 +27,19 @@ namespace BookExchange.Models
 
         public String? Published { get; set; }
 
-        [Required(ErrorMessage = "Book ISBN is Required")]
-        [StringLength(13, MinimumLength = 10, ErrorMessage = "ISBN must be between 10 and 13 characters")]
-        [RegularExpression(@"^[+]?[0-9]+$", ErrorMessage = "Only Numbers are allowed")]
-        [Display(Name = "ISBN")]
-        public String ISBN { get; set; }
-    }
+        [StringLength(13, MinimumLength = 13, ErrorMessage = "ISBN must be 13 characters long")]
+        [RegularExpression(@"^[+]?[0-9]+$", ErrorMessage = "Invalid ISBN-13 format")]
+        [Display(Name = "ISBN-13")]
+        public String ISBN13 { get; set; }
 
-    public class ClassUsed
-    {
-        //public int Id { get; set; }
-        public Guid ClassUsedID { get; set; }
-        public String className { get; set; }
-        public String ISBN { get; set; }
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "ISBN must be between 10 characters long")]
+        [RegularExpression(@"^[+]?[0-9xX]+$", ErrorMessage = "Invalid ISBN-10 format")]
+        [Display(Name = "ISBN-10")]
+        public String ISBN10 { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Classes")]
+        public virtual List<Guid> ClassIds { get; set; }
     }
 
     public class ISBNData
@@ -51,5 +58,14 @@ namespace BookExchange.Models
             Console.WriteLine("Subtitle: " + Subtitle);
             Console.WriteLine("PublishedDate: " + Published_date);
         }
+    }
+
+    public class ISBNdata
+    {
+        [Required]
+        [StringLength(13, MinimumLength = 10, ErrorMessage = "ISBN must be between 10 and 13 characters long")]
+        [RegularExpression(@"^[+]?[0-9xX]+$", ErrorMessage = "Invalid ISBN format")]
+        [Display(Name = "ISBN")]
+        public String ISBN { get; set; }
     }
 }
